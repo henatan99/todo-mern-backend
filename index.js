588cad8d7370aea0cd113b3043830b95ba62cc1a
Henok Mekonnen <henatan99@yahoo.com>
2021-10-19 17:16:48 +0300

    Modify index.js connecting it to database

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const routes = require('./routes/api');
+const path = require('path');
+require('dotenv').config()
 
 require('dotenv').config();
 
 const app = express();
 
 const port = process.env.PORT || 5000;
+
+// connect to the database
+mongoose.connect(process.env.DB, { useNewUrlParser: true })
+.then(() => console.log(`Database connected successfully`))
+.catch(err => console.log());
+
+// since mongoose promise is depreciated, we overide it with 
+// node's promise
+mongoose.Promise = global.Promise;
 
 // To handle CORS related issues 
 app.use((req, res, next) => {
@@ -13,8 +27,13 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use((req, res, next) => {
-    res.send('Welcome to Express');
+app.use(bodyParser.json());
+
+app.use('/api', routes);
+
+app.use((err, req, res, next) => {
+    console.log(err);
+    next();
 });
 
 app.listen(port, () => {

fce02e4d3ee1d0068f238b7e11d9ce6d23cf40fd
Henok Mekonnen <henatan99@yahoo.com>
2021-10-19 18:49:35 +0300

    Add db env

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const routes = require('./routes/api');
 const path = require('path');
-require('dotenv').config()
+require('dotenv').config();
 
 require('dotenv').config();
 
